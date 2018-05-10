fa559fca92e7a6fac8b452bfd0d8de2c6d8edbf6
trquth <tranquthien@gmail.com>
2018-05-10 11:29:11 +0700

    CRUD Realm

--- sts_realm_swift_notebook/sts_realm_swift_notebook/StudyRealm/Controllers/StudyRealmSwiftViewController.swift
+++ sts_realm_swift_notebook/sts_realm_swift_notebook/StudyRealm/Controllers/StudyRealmSwiftViewController.swift
@@ -11,13 +11,18 @@ import RealmSwift
 
 class StudyRealmSwiftViewController: UIViewController {
     
+    let  realm  = try! Realm()
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       
         setUpViews()
         seed()
+        //getAllUsers()
+        //getUserBy(1)
+        //getTodosOfUser(1)
+         print(Realm.Configuration.defaultConfiguration.fileURL)
         
-        print(Realm.Configuration.defaultConfiguration.fileURL)
     }
     
     override func didReceiveMemoryWarning() {
@@ -27,19 +32,67 @@ class StudyRealmSwiftViewController: UIViewController {
     
     func seed() {
         
-        let realm = try! Realm()
-    
         try! realm.write {
-            let id = PrimaryKeyUtility.shareInstance.incrementID(User.self)
-            realm.create(User.self, value: [id,"Thien", 25])
+            var id = PrimaryKeyUtility.shareInstance.incrementID(Authority.self)
+            realm.create(Authority.self, value: [id,"Read"])
+            
+            id = PrimaryKeyUtility.shareInstance.incrementID(Authority.self)
+            realm.create(Authority.self, value: [id,"Write"])
+            
+            id = PrimaryKeyUtility.shareInstance.incrementID(Authority.self)
+            realm.create(Authority.self, value: [id,"Edit"])
+            
+            id = PrimaryKeyUtility.shareInstance.incrementID(Authority.self)
+            realm.create(Authority.self, value: [id,"Delete"])
         }
         
+        try! realm.write {
+            var id = PrimaryKeyUtility.shareInstance.incrementID(User.self)
+            var predicate = NSPredicate(format: "id IN { %d , %d}",1,2)
+            var roles = realm.objects(Authority.self).filter(predicate)
+            realm.create(User.self, value: [id,"Thien", 25, roles])
+            
+            id = PrimaryKeyUtility.shareInstance.incrementID(User.self)
+            predicate = NSPredicate(format: "id IN { %d , %d}",0,2)
+            roles = realm.objects(Authority.self).filter(predicate)
+            realm.create(User.self, value: [id,"Thien 2", 26, roles])
+            
+            id = PrimaryKeyUtility.shareInstance.incrementID(User.self)
+            predicate = NSPredicate(format: "id IN { %d , %d}",3,4)
+            roles = realm.objects(Authority.self).filter(predicate)
+            realm.create(User.self, value: [id,"Thien 3", 26, roles])
+        }
+
         try! realm.write{
             
-            let user = realm.objects(User.self).first
-            let id = PrimaryKeyUtility.shareInstance.incrementID(Todo.self)
-            realm.create(Todo.self, value: [id,"Seed 1","Note 1",Priority.High.rawValue,user])
+            for index in 0...20 {
+                let user = realm.objects(User.self).filter(NSPredicate(format : "id = %d", Int(arc4random_uniform(4)))).first
+                let id = PrimaryKeyUtility.shareInstance.incrementID(Todo.self)
+                let title = "\(index) - Study Realm Practice"
+                let note = "This demo use for beginner"
+                realm.create(Todo.self, value: [id,title,note,Priority.High.rawValue,user])
+            }
+            
         }
+    }
+    
+    func getAllUsers() -> [User] {
+        let data = realm.objects(User.self)
+        print("Data ---->", data.count != 0 ? data[0] : "Empty")
+        return Array(data)
+    }
+    
+    func getUserBy(_ id : Int) -> User? {
+        let data = realm.object(ofType: User.self, forPrimaryKey: id)
+        print("Data ---->", data ??  "Empty")
+        return data
+    }
+    
+    func getTodosOfUser(_ userId : Int) -> [Todo] {
+        let predicate = NSPredicate(format: "user.id = %d ", userId)
+        let data = realm.objects(Todo.self).filter(predicate)
+        print("Data ---->", data.count != 0 ? data[0] : "Empty")
+        return Array(data)
     }
     
     let helloWorld : UILabel = {

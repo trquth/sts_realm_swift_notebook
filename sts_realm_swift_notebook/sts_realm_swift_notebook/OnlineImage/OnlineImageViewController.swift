efc5816b3f3b6bf3a949e3a3d4c3372117a95a0e
trquth <tranquthien@gmail.com>
2018-05-25 13:20:43 +0700

    Download image from web

--- sts_realm_swift_notebook/sts_realm_swift_notebook/OnlineImage/OnlineImageViewController.swift
+++ sts_realm_swift_notebook/sts_realm_swift_notebook/OnlineImage/OnlineImageViewController.swift
@@ -13,7 +13,7 @@ class OnlineImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +21,32 @@ class OnlineImageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+   
+    
+    lazy var imageView : UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        
+        image.image = UIImage(named:"swift")
+        
+        return image
+    }()
+    
+    
+    func setupViews() {
+        
+        view.backgroundColor = .white
+        view.addSubview(imageView)
+        imageView.imageFromServerURL(urlString: "https://picsum.photos/200/300")
+        
+        let views = ["v1" : imageView]
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[v1(80)]", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[v1(80)]", options: [], metrics: nil, views: views))
+        
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
-    */
 
 }
--- sts_realm_swift_notebook/sts_realm_swift_notebook/OnlineImage/UIImageExtension.swift
+++ sts_realm_swift_notebook/sts_realm_swift_notebook/OnlineImage/UIImageExtension.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIImageView {
+    
+    public func imageFromServerURL(urlString: String) {
+        
+        let urlString = URL(string: urlString)
+        guard let url = urlString  else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> Void in
+            
+            if error != nil {
+                return
+            }
+            DispatchQueue.main.async(execute: { () -> Void in
+                let image = UIImage(data: data!)
+                self.image = image
+            })
+            
+        }).resume()
+    }
+
+}
